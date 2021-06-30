37016bd0bea68728085464f8fd04fd36db47dae8
PhamQuocHuy12 <1801040104@s.hanu.edu.vn>
2021-06-30 11:22:16 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, SafeAreaView } from 'react-native';
+
+const question = [{question: 'This is a very long question ................................................................................',
+                    answers:[{option:'A',correct: true},
+                              {option:'B',correct: false},
+                              {option:'C',correct: true},
+                              {option:'D',correct: false}]},
+                  {question: 'This second long question ................................................................................',
+                    answers:[{option:'A',correct: true},
+                              {option:'B',correct: false},
+                              {option:'C',correct: false},
+                              {option:'D',correct: false}]}];
+
+var savedAnswer = new Array(question.length).fill(null);
 
 export default function App() {
+  const [selectedId, setSelectedId] = useState(null);
+  const [currentQuiz, setCurrentQuiz] = useState(0);
+  console.log(savedAnswer);
+
+  const nextQuestion = () => {
+    if(currentQuiz < question.length -1){
+      setCurrentQuiz(currentQuiz + 1);
+      if (savedAnswer[currentQuiz+1] == null){
+        setSelectedId(null);
+      } else {
+        setSelectedId(savedAnswer[currentQuiz+1]);
+      }
+    }
+  };
+
+  const previousQuestion = () => {
+    if(currentQuiz > 0){
+      setCurrentQuiz(currentQuiz - 1);
+      if (savedAnswer[currentQuiz-1] == null){
+        setSelectedId(null);
+      } else {
+        setSelectedId(savedAnswer[currentQuiz-1]);
+      }
+    }
+  };
+  
+  const onOptionPress = (option) => {
+      setSelectedId(option);
+      savedAnswer[currentQuiz] = option;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style = {styles.question}>{question[currentQuiz].question}</Text>
+      <View  style={styles.optionsContainer}>
+        <FlatList 
+          data ={question[currentQuiz].answers}
+          renderItem = {({item}) => (
+            <Option 
+                item={item}
+                onPress={() => onOptionPress(item.option)}
+                backgroundColor={item.option == selectedId? (item.correct? 'lightgreen': 'pink'):'grey'}
+                >
+            </Option>
+          )}
+          keyExtractor={(item) => item.option}
+          extraData={selectedId}>
+        </FlatList>
+      </View>
+      <View style = {styles.navigatorView}>
+
+        <TouchableOpacity style={styles.navButton} onPress={previousQuestion}>
+          <Text style={{textAlign:'center'}}> Prev Quiz </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.navButton} onPress={nextQuestion}>
+          <Text style={{textAlign:'center'}}> Next Quiz </Text>
+        </TouchableOpacity>
+
+      </View>
+      <StatusBar style="auto"/>
     </View>
   );
 }
+
+const Option = ({item, onPress, backgroundColor }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.option, backgroundColor={backgroundColor}]} >
+    <Text>{item.option}: {item.correct?'true': 'false'}</Text>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +93,37 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  question: {
+    alignSelf: 'baseline',
+    backgroundColor: 'lightblue',
+    width: '90%',
+    borderRadius: 20,
+    padding: '5%',
+    margin:'5%',
+  },
+  optionsContainer:{
+    width: '90%',
+  },
+  option:{
+    alignSelf: 'baseline',
+    width: '90%',
+    marginLeft: '5%',
+    marginEnd: '5%',
+    marginTop: '2%' ,
+    borderRadius: 20,
+    padding: '5%',
+  },
+  navButton:{
+    marginTop: '5%',
+    borderRadius: 20,
+    padding:'5%',
+    backgroundColor: 'lightblue',
+    width: '35%',
+  },
+  navigatorView:{
+    width: '96%',
+    flexDirection:'row',
+    justifyContent:'space-around',
   },
 });
