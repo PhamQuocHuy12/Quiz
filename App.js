b41f9fc80ae0cf5301366a117f5fd785fe7bb95a
PhamQuocHuy12 <1801040104@s.hanu.edu.vn>
2021-06-30 21:33:08 +0700

    update screen

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 const question = [{question: 'This is a very long question ................................................................................',
-                    answers:[{option:'A',correct: true},
-                              {option:'B',correct: false},
-                              {option:'C',correct: true},
-                              {option:'D',correct: false}]},
-                  {question: 'This second long question ................................................................................',
-                    answers:[{option:'A',correct: true},
-                              {option:'B',correct: false},
-                              {option:'C',correct: false},
-                              {option:'D',correct: false}]}];
+                    answers:[{id:'A',text:'something',correct: true},
+                              {id:'B',text:'something',correct: false},
+                              {id:'C',text:'something',correct: false},
+                              {id:'D',text:'something',correct: false}]},
+                  {question: 'This is second long question ................................................................................',
+                    answers:[{id:'A',text:'something',correct: true},
+                              {id:'B',text:'something',correct: false},
+                              {id:'C',text:'something',correct: false},
+                              {id:'D',text:'something',correct: false}]},
+                  {question: 'This is third long question ................................................................................',
+                    answers:[{id:'A',text:'something',correct: false},
+                              {id:'B',text:'something',correct: false},
+                              {id:'C',text:'something',correct: false},
+                              {id:'D',text:'something',correct: true}]}];
 
-var savedAnswer = new Array(question.length).fill({option: null, correct: null});
+var savedAnswer = new Array(question.length).fill({id: null, correct: null});
 
 export default function App() {
   const [selectedId, setSelectedId] = useState(null);
@@ -23,10 +28,10 @@ export default function App() {
   const nextQuestion = () => {
     if(currentQuiz < question.length -1){
       setCurrentQuiz(currentQuiz + 1);
-      if (savedAnswer[currentQuiz+1].option == null){
+      if (savedAnswer[currentQuiz+1].id == null){
         setSelectedId(null);
       } else {
-        setSelectedId(savedAnswer[currentQuiz+1].option);
+        setSelectedId(savedAnswer[currentQuiz+1].id);
       }
     }
   };
@@ -34,22 +39,22 @@ export default function App() {
   const previousQuestion = () => {
     if(currentQuiz > 0){
       setCurrentQuiz(currentQuiz - 1);
-      if (savedAnswer[currentQuiz-1].option == null){
+      if (savedAnswer[currentQuiz-1].id == null){
         setSelectedId(null);
       } else {
-        setSelectedId(savedAnswer[currentQuiz-1].option);
+        setSelectedId(savedAnswer[currentQuiz-1].id);
       }
     }
   };
   
   const onOptionPress = (option) => {
-      setSelectedId(option.option);
-      savedAnswer[currentQuiz] = option;
+      setSelectedId(option.id);
+      savedAnswer[currentQuiz] = {id: option.id, correct: option.correct};
   }
 
   return (
     <View style={styles.container}>
-      <Text style = {styles.question}>{question[currentQuiz].question}</Text>
+      <Text style = {styles.question}>Question {currentQuiz+1}: {question[currentQuiz].question}</Text>
       <View  style={styles.optionsContainer}>
         <FlatList 
           data ={question[currentQuiz].answers}
@@ -57,24 +62,26 @@ export default function App() {
             <Option 
                 item={item}
                 onPress={() => onOptionPress(item)}
-                backgroundColor={item.option == selectedId? '#6495ED':'#fff'}
+                backgroundColor={item.id == selectedId? '#6495ED':'#fff'}
                 >
             </Option>
           )}
-          keyExtractor={(item) => item.option}
+          keyExtractor={(item) => item.id}
           extraData={selectedId}>
         </FlatList>
       </View>
       <View style = {styles.navigatorView}>
-
-        <TouchableOpacity style={styles.navButton} onPress={previousQuestion}>
-          <Text style={{textAlign:'center'}}> Prev Quiz </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.navButton} onPress={nextQuestion}>
-          <Text style={{textAlign:'center'}}> Next Quiz </Text>
-        </TouchableOpacity>
-
+        {currentQuiz > 0 &&
+          <TouchableOpacity style={styles.navButton} onPress={previousQuestion}>
+            <Text style={{textAlign:'center'}}> Prev Quiz </Text>
+          </TouchableOpacity>
+        }
+        {currentQuiz < question.length -1 &&
+          <TouchableOpacity style={styles.navButton} onPress={nextQuestion}>
+            <Text style={{textAlign:'center'}}> Next Quiz </Text>
+          </TouchableOpacity>
+        }
+        
       </View>
       <StatusBar style="auto"/>
     </View>
@@ -83,7 +90,7 @@ export default function App() {
 
 const Option = ({item, onPress, backgroundColor }) => (
   <TouchableOpacity onPress={onPress} style={[styles.option, backgroundColor={backgroundColor}]} >
-    <Text>{item.option}: {item.correct?'true': 'false'}</Text>
+    <Text>{item.id}: {item.text} {item.correct?'true': 'false'}</Text>
   </TouchableOpacity>
 );
 
@@ -119,8 +126,10 @@ const styles = StyleSheet.create({
   navButton:{
     marginTop: '5%',
     borderRadius: 20,
+    borderWidth: 5,
+    borderColor:'#6495ED',
     padding:'5%',
-    backgroundColor: 'lightblue',
+    backgroundColor: '#87CEFA',
     width: '35%',
   },
   navigatorView:{
